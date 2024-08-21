ab8053c72c55e0193832fab5fc8252d4c7ea2755
emersson <emerssonramirez510@gmail.com>
2024-08-21 14:24:17 -0500

    iniciando proyecto con estilos tailwind y almacenamiento en cache con react-query

--- front-pmachinapp/src/main.jsx
+++ front-pmachinapp/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// manejo de rutas
+import { BrowserRouter } from "react-router-dom";
+
+// almacenamiento en cache con react-query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+import {} from "@tanstack/react-query";
+
+createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  </QueryClientProvider>
+);
